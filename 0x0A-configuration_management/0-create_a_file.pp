dc4e3bcb49c26fd24ae4cd5e42b8fd94cc5db701
ThunderCharm <ogunyadeolamilekan2020@gmail.com>
2024-04-23 18:38:00 -0700

    fgf

--- 0x0A-configuration_management/0-create_a_file.pp
+++ 0x0A-configuration_management/0-create_a_file.pp
@@ -1,7 +1,9 @@
 #create a file
 file { '/tmp/school':
-       mode => '0744',
-       owner => 'www-data',
-       group => 'www-data',
-       content => 'I love Puppet'
+  ensure  => file,
+  mode    => '0744',
+  owner   => 'www-data',
+  group   => 'www-data',
+  content => 'I love Puppet',
 }
+
